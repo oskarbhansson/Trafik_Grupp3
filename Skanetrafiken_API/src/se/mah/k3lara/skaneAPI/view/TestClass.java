cf24d0caf1fda8090ea6e14e50b65ce6b7b599ae
Sewic <bassewic@outlook.com>
2016-03-16 09:37:53 +0100

    SkånetrafikenAPI
    Metoder osv

---
+++ Skanetrafiken_API/src/se/mah/k3lara/skaneAPI/view/TestClass.java
@@ -0,0 +1,64 @@
+package se.mah.k3lara.skaneAPI.view;
+
+import java.util.ArrayList;
+import java.util.Calendar;
+
+import se.mah.k3lara.skaneAPI.control.Constants;
+import se.mah.k3lara.skaneAPI.model.Journey;
+import se.mah.k3lara.skaneAPI.model.Journeys;
+import se.mah.k3lara.skaneAPI.model.Line;
+import se.mah.k3lara.skaneAPI.model.Lines;
+import se.mah.k3lara.skaneAPI.model.Station;
+import se.mah.k3lara.skaneAPI.xmlparser.Parser;
+
+public class TestClass {
+
+	public static void main(String[] args) {
+
+		/** Deklarerar en string som hämtar värden från constantsKlassen. */
+		String searchURL = Constants.getURL("80000", "81216", 20); // Malmö C =
+																	// 80000,
+																	// Lund C,
+																	// 81216
+																	// Malmö
+																	// Gatorg
+																	// 80100,
+																	// Hässleholm
+																	// C 93070
+		System.out.println(searchURL);
+		System.out.println("// Results when searching:");
+		/**
+		 * Strängen används i journey klassen och itererar genom en array som
+		 * hämtar data från skånetrafiken.
+		 */
+		Journeys journeys = Parser.getJourneys(searchURL);
+		for (Journey journey : journeys.getJourneys()) {
+			System.out.print(journey.getStartStation() + " - ");
+			System.out.print(journey.getEndStation());
+			String time = journey.getDepDateTime().get(Calendar.HOUR_OF_DAY) + ":"
+					+ journey.getDepDateTime().get(Calendar.MINUTE);
+			System.out.println(" Departs " + time + " that is in " + journey.getTimeToDeparture()
+					+ " minutes. And it is " + journey.getDepTimeDeviation() + " min late");
+		}
+
+		System.out.println("// Stations when searching for stations containing \"Malm\"");
+
+		/**
+		 * Deklarerar en string-array av klassen Station som sedan jämför en
+		 * sträng med objecten som finns arrayn, vilket sedan printar ut
+		 * stationer som liknar given sträng med stationsnummer och namn.
+		 */
+		ArrayList<Station> searchStations = new ArrayList<Station>();
+		searchStations.addAll(Parser.getStationsFromURL("Malm"));
+		for (Station s : searchStations) {
+			System.out.println(s.getStationName() + " number:" + s.getStationNbr());
+		}
+
+		System.out.println("// Busses departing from Ubåtshallen stationsnummer 80046 ");
+		Lines lines = Parser.getStationResults(new Station("80046"));
+		for (Line l : lines.getLines()) {
+			System.out.println("Line " + l.getLine() + " departs: " + l.getDepTime().get(Calendar.HOUR_OF_DAY) + ":"
+					+ l.getDepTime().get(Calendar.MINUTE) + " and is " + l.getDepTimeDeviation() + " minutes late");
+		}
+	}
+}
