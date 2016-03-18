f55051cb01822b53687c69e5d11d748a5f0beade
joppepoppe <jonatan.holmgren@hotmail.com>
2016-03-18 12:13:53 +0100

    Fix

--- Skanetrafiken_API/src/timeTableLogic.java
+++ Skanetrafiken_API/src/timeTableLogic.java
@@ -5,76 +5,44 @@ import se.mah.k3lara.skaneAPI.model.Journey;
 import se.mah.k3lara.skaneAPI.model.Journeys;
 import se.mah.k3lara.skaneAPI.xmlparser.Parser;
 
-
-
 public class timeTableLogic {
-	
-	
+
 	private String[] lineArray = new String[10];
-	
-	
-	public timeTableLogic(){
-		lineArray[0] = Constants.getURL("80000", "80110" ,3);
-		lineArray[1] = Constants.getURL("80000", "80002" ,3);
-		
+
+	public timeTableLogic() {
+		lineArray[0] = Constants.getURL("80000", "80110", 3);
+		lineArray[1] = Constants.getURL("80000", "80002", 3);
+
 	}
-	
-	
-	public class getJourney extends Thread{
-		
-		
-		
+
+	public class getVarnhem extends Thread {
 		@Override
-		public void run(){
-			
-			
-			
+		public void run() {
 			String searchURL = lineArray[0];
-			
-			
 			Journeys journeys = Parser.getJourneys(searchURL);
-			
-			
+
 			for (Journey journey : journeys.getJourneys()) {
-				int fyran1 = 4;
-				int åttan = 8;
-				int femman = 5;
+				int fyran = 4;
 				
-				
-				String time = journey.getDepDateTime().get(Calendar.HOUR_OF_DAY)+":"+journey.getDepDateTime().get(Calendar.MINUTE);
-				
+				String time = journey.getDepDateTime().get(Calendar.HOUR_OF_DAY) + ":"
+						+ journey.getDepDateTime().get(Calendar.MINUTE);
+
 				try {
-					if (Integer.parseInt(journey.getLineOnFirstJourney()) == fyran1){
+					if (Integer.parseInt(journey.getLineOnFirstJourney()) == fyran) {
 						System.out.println(journey.getLineOnFirstJourney());
-						
 						System.out.println(journey.getEndStation().toString());
-						
-						
-						System.out.println(" Departs " + time + " " +  " that is in "+journey.getTimeToDeparture()+ " minutes. And it is "+journey.getDepTimeDeviation()+" min late");
-						
+						System.out.println(" Departs " + time + " " + " that is in " + journey.getTimeToDeparture()
+								+ " minutes. And it is " + journey.getDepTimeDeviation() + " min late");
+
 					}
-					else if (Integer.parseInt(journey.getLineOnFirstJourney()) == femman){
-System.out.println(journey.getLineOnFirstJourney());
-						
-						System.out.print(journey.getStartStation()+" - ");
-						
-						System.out.println(" Departs " + time + " " +  " that is in "+journey.getTimeToDeparture()+ " minutes. And it is "+journey.getDepTimeDeviation()+" min late");
-						
-					}
-					
-					
-				} catch(java.lang.NumberFormatException e) {
+
+				} catch (java.lang.NumberFormatException e) {
 					System.out.println("error");
-					
+
 				}
-			
-			
-				
 			}
-			}
-			
-			
-			
 		}
+
+	}
 
 }
