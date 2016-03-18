b72af6d397f8d7bb24205c4271a26316577ff594
joppepoppe <jonatan.holmgren@hotmail.com>
2016-03-18 13:32:37 +0100

    Yes

--- Skanetrafiken_API/src/se/mah/k3lara/skaneAPI/view/timeTableLogic.java
+++ Skanetrafiken_API/src/se/mah/k3lara/skaneAPI/view/timeTableLogic.java
@@ -1,17 +1,19 @@
+package se.mah.k3lara.skaneAPI.view;
+
 import java.util.Calendar;
 
 import se.mah.k3lara.skaneAPI.control.Constants;
 import se.mah.k3lara.skaneAPI.model.Journey;
 import se.mah.k3lara.skaneAPI.model.Journeys;
+import se.mah.k3lara.skaneAPI.view.SkanetrafikenGUI;
 import se.mah.k3lara.skaneAPI.xmlparser.Parser;
 
 public class timeTableLogic {
 
-	
+	private SkanetrafikenGUI trafikGUI;
 
 	public timeTableLogic() {
 		
-
 	}
 
 	public class getSegevang extends Thread {
@@ -47,7 +49,7 @@ public class timeTableLogic {
 	public class getBernstorp extends Thread {
 		@Override
 		public void run() {
-			String searchURL = Constants.getURL("80000", "31033", 3);
+			String searchURL = Constants.getURL("80000", "80200", 20);
 			Journeys journeys = Parser.getJourneys(searchURL);
 
 			for (Journey journey : journeys.getJourneys()) {
@@ -58,11 +60,11 @@ public class timeTableLogic {
 
 				try {
 					if (Integer.parseInt(journey.getLineOnFirstJourney()) == fyran) {
-						System.out.println(journey.getLineOnFirstJourney());
-						System.out.println(journey.getEndStation().toString());
-						System.out.println(" Departs " + time + " " + " that is in " + journey.getTimeToDeparture()
+						skanetrafikenGUI.textField_4.setText(journey.getLineOnFirstJourney());
+						skanetrafikenGUI.textField_2.setText(journey.getEndStation().toString());
+						skanetrafikenGUI.textField_5.setText(" Departs " + time + " " + " that is in " + journey.getTimeToDeparture()
 								+ " minutes. And it is " + journey.getDepTimeDeviation() + " min late");
-
+						System.out.println("HEJ");
 					}
 
 				} catch (java.lang.NumberFormatException e) {
@@ -73,5 +75,7 @@ public class timeTableLogic {
 		}
 
 	}
+	
+	SkanetrafikenGUI skanetrafikenGUI = new SkanetrafikenGUI();
 
 }
